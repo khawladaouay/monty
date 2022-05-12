833567d17c194c49a5045fad727f7ed95a632111
khawladaouay <daouaykhawla782@gmail.com>
2022-05-12 11:05:15 +0100

    task 1

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ *pint - prints the top value of the stack
+ *@stack: pointer to the top of the stack
+ *@line_number: the line number of the command interpted
+ *
+ *Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
